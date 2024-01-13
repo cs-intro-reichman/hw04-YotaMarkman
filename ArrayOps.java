e5ff242224a077a43e280dec1e92e4451767e9f3
YotaMarkman <yotam.markman@post.runi.ac.il>
2024-01-13 16:56:44 +0200

    HW4

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,113 @@
-public class ArrayOps {
-    public static void main(String[] args) {
+public class ArrayOps 
+{
+    public static void main(String[] args) 
+    {
         
     }
-    
-    public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+    public static int findMissingInt(int[] array) 
+    {
+        int missing = -1;
+        int temp;
+        for (int i = 0; i < array.length; i++) 
+        {
+            temp = array[i];
+            for (int j = i; j < array.length; j++) 
+            {
+                if (temp > array[j]) 
+                {
+                    array[i] = array[j];
+                    array[j] = temp;
+                    temp = array[i];
+                }
+            }
+        }
+        for (int i = 0; i < array.length; i++) 
+        {
+             if (array[i] != i) 
+            {
+                missing = i;
+                break;
+            }
+        }
+        return missing;
     }
-
-    public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+    public static int secondMaxValue(int[] array) 
+    {
+        int[] ordered = new int[array.length];
+        for (int i = 0; i < ordered.length; i++) 
+        {
+            ordered[i] = array[i];
+        }
+        for (int i = 0; i < ordered.length - 1; i++) 
+        {
+            for (int j = i + 1; j < ordered.length; j++) 
+            {
+                if (ordered[i] > ordered[j]) 
+                {
+                    int temp = ordered[i];
+                    ordered[i] = ordered[j];
+                    ordered[j] = temp;
+                }
+            }
+        }
+        if (ordered.length >= 2) 
+        {
+            return ordered[ordered.length - 2];
+        } 
+        else 
+        {
+            return -1;
+        }
     }
-
-    public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+    public static boolean containsTheSameElements(int [] array1,int [] array2) 
+    {
+        int count = 0;
+        for ( int i = 0; i < array1.length; i++)
+        {
+            count = 0;
+            for ( int j = 0; j < array2.length; j++)
+            {
+                if ( array1[i] != array2[j] )
+                {
+                    count++;
+                }
+            }
+            if ( count == array2.length)
+            {
+                 return false;
+            }
+        }
+        return true;
     }
-
-    public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+    public static boolean isSorted(int [] array)
+    {
+        int countup = 0;
+        int countdown = 0;
+        int temp4;
+        for ( int i = 0; i < array.length; i++)
+        {
+            temp4 = array[i];
+            for ( int j = i+1; j < array.length; j++)
+            {
+                if ( temp4 <= array[j])
+                {
+                    temp4 = array[j];
+                    countup++;
+                }
+                else
+                {
+                    temp4 = array[j];
+                    countdown++;
+                }
+            }
+        }
+        if ( ( countdown > 0 && countup > 0))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }        
     }
-
 }
--- StringOps.java
+++ StringOps.java
@@ -21,22 +21,99 @@ public class StringOps {
     //////        in Recitation 3 question 5             ///////
     //////                                               ///////
     ////////////////////////////////////////////////////////////
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) 
+    {
     }
-
-    public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+    public static String capVowelsLowRest (String string) 
+    {
+        char lettercheck;
+        String result = "";
+        for ( int i = 0; i < string.length(); i++ )
+        {
+            lettercheck = string.charAt(i);
+            if( lettercheck == 97 || lettercheck == 101 || lettercheck == 105 || lettercheck == 111 || lettercheck == 117 )
+            {
+                result = result + (char)(lettercheck - 32);	
+            }
+            else if ( ( lettercheck > 64 && lettercheck < 91 ) && ( lettercheck != 65 || lettercheck != 69 || lettercheck != 73 || lettercheck != 79 || lettercheck != 85))
+            {
+                result = result + (char)(lettercheck + 32);	
+            }
+            else
+            {
+                result = result + (char)(lettercheck);
+            }
+        }
+        return result;
     }
-
-    public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+    public static String camelCase (String string)
+    {
+        char check;
+        char checkspace;
+        String finalresult = "";
+        for( int i = 0; i < string.length(); i++)
+        {
+            check = string.charAt(i);
+            if ( check > 64 && check < 91)
+            {
+                finalresult = finalresult + (char)(check+32);
+                break;
+            }
+            else
+            {
+                finalresult = finalresult + (char)(check);
+                break;
+            }
+        }
+         for ( int i = 1; i < string.length(); i++)
+        {
+            check = string.charAt(i);
+            checkspace = string.charAt(i+1);
+            if( check == 32&& checkspace > 32)
+            {
+                if ( checkspace > 64 && checkspace < 91)
+                {
+                    finalresult = finalresult + (char)(checkspace);
+                    break;
+                }
+                else
+                {
+                    finalresult = finalresult + (char)(checkspace-32);
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < string.length(); i++) 
+        {
+            char c = string.charAt(i);
+            if (c == ' ') 
+            {
+                continue;
+            }
+            finalresult += c;
+        }
+        return finalresult;
     }
-
-    public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+    public static int[] allIndexOf(String string, char chr) 
+    {
+        int count = 0;
+        for (int i = 0; i < string.length(); i++) 
+        {
+            if (chr == string.charAt(i)) 
+            {
+                count++;
+            }
+        }
+        int[] repeated = new int[count];
+        int index = 0;
+        for (int i = 0; i < string.length(); i++) 
+        {
+            if (chr == string.charAt(i)) 
+            {
+                repeated[index] = i;
+                index++;
+            }
+        }
+        return repeated;
     }
 }
